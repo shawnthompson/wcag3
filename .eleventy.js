5144ee9854f705190dbc597ca534fcf849f4cb2e
Shawn Thompson <plansmash@me.com>
2023-12-29 12:18:56 -0500

    organized files into _src directory

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,33 @@
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-const listOutcomes = require('./how-to/build/list-outcomes');
+
+// Disabled for now
+// const listOutcomes = require('./how-to/build/list-outcomes');
 
 module.exports = function(eleventyConfig) {
-  // Ignore a couple things
-	eleventyConfig.ignores.add('./*.md');
-	eleventyConfig.ignores.add('**/readme.md');
-	eleventyConfig.ignores.add('_build/**');
+
+	// Ignore a couple things
 	eleventyConfig.ignores.add('**/_template/**');
 
-  // Global data
-  eleventyConfig.addGlobalData("layout", "layout.html");
-  eleventyConfig.addGlobalData("./how-to/outcomes", listOutcomes().outcomes);
+	// Global data not needed right now
+	//   eleventyConfig.addGlobalData("layout", "layout.html");
+	//   eleventyConfig.addGlobalData("./how-to/outcomes", listOutcomes().outcomes);
 
-  // Make it easy to deploy to gh-pages
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
+	// Make it easy to deploy to gh-pages
+	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 
-  // Copy `assets/` to `_site/js`
-  eleventyConfig.addPassthroughCopy({"./how-to/assets" : "/assets"});
+	// Copy files to _site folder
+	eleventyConfig.addPassthroughCopy({"./_src/assets" : "assets"});
+	eleventyConfig.addPassthroughCopy({"./_src/js" : "js"});
+	eleventyConfig.addPassthroughCopy("acknowledgements"); 
+	eleventyConfig.addPassthroughCopy({"./_src/css" : "css"}); 
 
-  // Copy `js/` to `_site/js`
-  eleventyConfig.addPassthroughCopy("js");
-
-  // Copy `acknowledgements/` to `_site/acknowledgements`
-  eleventyConfig.addPassthroughCopy("acknowledgements"); 
-
-  // Copy `img/` to `_site/css/img`
-  eleventyConfig.addPassthroughCopy({"./img" : "/css/img"}); 
-
-  const dir = {
-    input: "./",
-    output : "_site",
-    includes: "./how-to/_includes" // relative to dir.input
-  }
+	const dir = {
+		input: "./_src",
+		output : "_site",
+		includes: "_includes" // relative to dir.input
+	}
   
-  return { 
+	return { 
 		dir,
 		templateFormats : ["html", "md", "njk", "css"],
 		htmlTemplateEngine : "njk",
